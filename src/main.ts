f5dbed7d695417029e95afc43043f106244b2140
Gabrielfernandes87f <gabriel1coder@gmail.com>
2023-04-22 12:13:28 -0300

    [chore:app] add config de cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      optionsSuccessStatus: 200,
+    }),
+  );
+
   await app.listen(4000);
 }
 bootstrap();
